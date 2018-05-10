cb8136274d86b1308c0c3f0de5acaffd93d6692c
Devon Norris <norris.devon@gmail.com>
2018-05-10 20:21:22 +0000

    tough recursion is

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 function sumItems(array) {
-  // Sum all the numbers in the array
+    let answer = 0;
+
+    for (let i = 0; i < array.length; i++) {
+        if (Array.isArray(array[i])) {
+            answer += sumItems(array[i])
+        } else {
+            answer += array[i];
+        }
+    }
+    return answer;
 }
+
 
 module.exports = sumItems;
